507a51db487b89dd63636a8360080b6edb466827
MansiThakkar147 <rivavyacontrol@gmail.com>
2026-08-09 17:32:07 +0530

    Add Google Tag Manager (GTM-WJ348W2P) container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,15 @@ export default function RootLayout({
       className={`${cormorant.variable} ${poppins.variable} ${inter.variable} h-full antialiased`}
     >
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WJ348W2P');
+          `}
+        </Script>
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-9DLW0ZK5EB"
@@ -59,6 +68,14 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground overflow-x-hidden">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJ348W2P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personJsonLd) }}
